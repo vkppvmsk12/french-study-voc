c98054e257cce2aeaf2b6414111910ba9659b612
Vihaan Kembhavi <vihaankembhavi12@gmail.com>
2024-06-08 19:59:53 +0200

    Skips empty lines

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ word_list = {}
 # Splitting the line into word and translation
 with open("main.txt", "r", encoding="utf-8") as main:
     for line in main.readlines():
-        word_list[line.split("=")[0].strip()] = line.split("=")[1].strip()
+        if line:
+            word_list[line.split("=")[0].strip()] = line.split("=")[1].strip()
 
 maximum_words = len(word_list)
 
